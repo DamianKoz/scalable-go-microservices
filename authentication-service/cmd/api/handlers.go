b224529299fcf4dbca4534511bca0ac456432d90
Damian <damian.makoza@gmail.com>
2023-07-23 10:04:54 +0200

    Added logger and mail service

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -20,13 +22,20 @@ func (app *Config) authenticate(w http.ResponseWriter, r *http.Request) {
 
 	user, err := app.Models.User.GetByEmail(requestPayload.Email)
 	if err != nil {
-		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusUnauthorized)
 		return
 	}
 
 	valid, err := user.PasswordMatches(requestPayload.Password)
 	if err != nil || !valid {
-		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusUnauthorized)
+		return
+	}
+
+	// log authentication
+	err = app.logRequest("authentication", fmt.Sprintf("%s logged in", user.Email))
+	if err != nil {
+		app.errorJSON(w, errors.New("failed to log"), http.StatusBadRequest)
 		return
 	}
 
@@ -37,4 +46,34 @@ func (app *Config) authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app.writeJson(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) logRequest(name, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, err := json.Marshal(entry)
+	if err != nil {
+		return err
+	}
+
+	logServiceUrl := "http://logger-service/log"
+
+	request, err := http.NewRequest(http.MethodPost, logServiceUrl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+	return nil
 }
---
+++ logger-service/logger-service.dockerfile
@@ -0,0 +1,20 @@
+# Not needed, because the build is happening in the makefile
+# FROM golang:1.18-alpine as builder
+
+# RUN mkdir /app
+
+# COPY . /app
+
+# WORKDIR /app
+
+# RUN CGO_ENABLED=0 go build -o brokerApp ./cmd/api
+
+# RUN chmod +x /app/brokerApp
+
+FROM alpine:latest
+
+RUN mkdir /app
+
+COPY loggerServiceApp /app
+
+CMD ["/app/loggerServiceApp"]
